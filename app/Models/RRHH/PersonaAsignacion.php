e38f7fb8add408b24631e1536a0c050c41b995db
betosky <ecep2019@endor.iie.cl>
2019-11-29 18:31:48 -0300

    Modelo: Persona Asignacion

---
+++ app/Models/RRHH/PersonaAsignacion.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models\RRHH;
+use Illuminate\Database\Eloquent\Model;
+
+class PersonaAsignacion extends Model
+{
+	protected $table = 'rrhh.persona_asignacion';
+	protected $primaryKey  = 'id_persona_asignacion';
+
+}
